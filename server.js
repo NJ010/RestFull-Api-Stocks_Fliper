91a18fcb53163992e89be218b38ac60283e3b5e0
Naman Jain <jainnaman335@gmail.com>
2020-08-14 22:50:25 +0530

    ready to use

--- server.js
+++ server.js
@@ -2,27 +2,8 @@ const http =require("http");
 const app =require("./app");
 const mongoose =require("mongoose");
 
-
-
-
 const port =process.env.PORT || 3000;
+
 const server=http.createServer(app);
-
-
-
-// app.use()
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 server.listen(port);
